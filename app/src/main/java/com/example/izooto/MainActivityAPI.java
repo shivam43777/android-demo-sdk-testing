696850e27ed7e07970a4ed471b89a346cb9e2f10
skm <shivam@izooto.com>
2022-05-11 10:31:39 +0530

    json call to object and array then API CALL

---
+++ app/src/main/java/com/example/izooto/MainActivityAPI.java
@@ -0,0 +1,14 @@
+package com.example.izooto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityAPI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_api);
+    }
+}
