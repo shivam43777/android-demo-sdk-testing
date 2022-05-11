90f1165b925f25be30a2b267695d5a320e7ff0f4
skm <shivam@izooto.com>
2022-05-11 10:46:06 +0530

    api

--- app/src/main/java/com/example/izooto/CustomAdapter.java
+++ app/src/main/java/com/example/izooto/CustomAdapter.java
@@ -1,2 +1,57 @@
-package com.example.izooto;public class CustomAdapter {
+package com.example.izooto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends BaseAdapter {
+
+    Context context;
+    String[] data;
+    private static LayoutInflater inflater = null;
+
+    public CustomAdapter(Context context, String[] data) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.data = data;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return data[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View vi = convertView;
+        if (vi == null)
+            vi = inflater.inflate(R.layout.textview, null);
+        TextView text = (TextView) vi.findViewById(R.id.text);
+        text.setText(data[position]);
+        return vi;
+    }
 }
